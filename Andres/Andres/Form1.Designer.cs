e0c8a164d067bea25e28c944c4034eb33ae9e67d
NightRoadIx <quintanarxbio@gmail.com>
2022-06-23 13:52:43 -0500

    Add files via upload

---
+++ Andres/Andres/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Andres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(149, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 126);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ami++";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 269);
+            this.textBox1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(610, 180);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 510);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(149, 510);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(410, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(832, 521);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(957, 578);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Andres/Andres/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Andres
+{
+    public partial class Form1 : Form
+    {
+
+        /* ATRIBUTOS */
+        bool encendido;
+        string cadenatmp2;
+        string Recibidos;
+        // Este es una clase para comunicación con Arduino
+        // mediante el puerto serial, esto es un objeto que
+        // representa a dicho puerto
+        System.IO.Ports.SerialPort ArduinoPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+
+            try
+            {
+                serialPort1.Open();
+                label1.Text = "Ami++";
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error mortal " + error.ToString(), "Oh no, oh no, oh no no no no no", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label1.Text = "Ami--";
+            }
+
+            /*
+            // Crear el objeto para el serial port
+            // Se hace la llamada al constructor...
+            // Al declaralo, le indicamos al compilador que se
+            // generará un objeto de dicho tipo
+            // (System.IO.Ports.SerialPort), pero hasta aquí
+            // ya se reserva la memoria
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            // Ahora si, tenemos acceso a los stributos del objeto
+            // que sirven para la configuración
+            ArduinoPort.PortName = "COM3";
+            ArduinoPort.BaudRate = 9600;
+            try
+            {
+                // Y se abre la comunicación con el método correspondiente
+                ArduinoPort.Open();
+                if (ArduinoPort.IsOpen)
+                {
+                    ArduinoPort.Write("a");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error mortal " + error.ToString(), "Título", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            */
+            button1.BackColor = Color.Black;
+            this.encendido = false;
+
+            timer1.Enabled = false;
+
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(Recepcion);
+            //this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+        }
+
+        private void Recepcion(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            this.cadenatmp2 = serialPort1.ReadExisting();
+
+            // Acumulasización
+            this.Recibidos += this.cadenatmp2;
+
+            // Invocas un proceso por tramas
+            this.Invoke(new EventHandler(Actualizar));
+        }
+
+        private void Actualizar(object s, EventArgs e)
+        {
+            textBox1.Text = Recibidos;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.encendido)
+            {
+                // Apaga el botón
+                button1.BackColor = Color.Black;
+                this.encendido = false;
+                //timer1.Enabled = false;
+            }
+            else
+            {
+                // prende el botón
+                button1.BackColor = Color.Red;
+                this.encendido = true;
+                //timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hola Andrés", "El Andrés", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //textBox1.Text += "Hola Andrés";
+            string a = ArduinoPort.ReadLine();
+            textBox1.Text = a;
+
+            string prueba = "21.98,34.67,666.0";
+
+            string[] andres = prueba.Split(',');
+            textBox1.Text += andres[0]; 
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            byte[] mBuffer = new byte[1];
+
+            label2.Text = trackBar1.Value.ToString();
+            mBuffer[0] = Convert.ToByte(trackBar1.Value);
+
+            serialPort1.Write(mBuffer, 0, mBuffer.Length);
+        }
+    }
+}
